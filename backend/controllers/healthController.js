f2dd5c2f4c0659b1a7a5315a2931c44e554f153c
dev.gsilv <dev.gsilv@gmail.com>
2023-08-15 16:01:40 -0300

    test: health check ci/cd test.

--- backend/controllers/healthController.js
+++ backend/controllers/healthController.js
@@ -1,6 +1,6 @@
 export const check = (req, res) => {
     try {
-        res.status(200).send('<h1>Health check: PASS!</h1>')
+        res.status(200).send('<h1>Health check: PASS! GitHub Action online!</h1>')
     } catch (e) {
         res.status(400).send(e)
     }
